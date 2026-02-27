c96eca54d540a2f49dc0c2f297e49386237a223a
Yakov Balashov <yakov.balashov@gmail.com>
2026-02-27 13:42:00 +0100

    feat: add OrbitParameters struct

---
+++ Assets/Scripts/OrbitParameters.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace ProceduralPlanets
+{
+    public struct OrbitParameters
+    {
+        public float MainRadius;
+        public float RadiusRatio;
+        public float Inclination;
+        public float Rotation;
+        public float SpeedInDegreesPerSecond;
+    }
+}

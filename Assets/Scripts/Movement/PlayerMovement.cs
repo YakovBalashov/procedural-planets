43ae2c1b9d70cb965e31edb1c36864947e26ec53
Yakov Balashov <yakov.balashov@gmail.com>
2026-04-08 18:13:59 +0200

    feat: implement player movement acceleration

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -11,26 +11,34 @@ namespace ProceduralPlanets.Movement
     {
         [Header("Movement Settings")]
         [SerializeField] private float orbitToBodyRadiusRatio = 1.5f;
+
         [SerializeField] private float velocity = 10f;
         [SerializeField] private float decelerationDistanceToRadiusRatio = 3f;
         [SerializeField] private float minVelocity = 0.1f;
         [SerializeField] private float orbitalVelocity = 2f;
-        
+
+        [SerializeField, Tooltip("Acceleration duration in seconds")]
+        private float accelerationDuration = 5f;
+
+
         [Header("Gizmos Settings")]
         [SerializeField] private Color trajectoryColor = Color.yellow;
+
         [SerializeField] private Color destinationColor = Color.orange;
         [SerializeField] private float destinationGizmoRadius = 5f;
-        
+
+
         [Header("System References")]
         [SerializeField] private SystemGenerator systemGenerator;
+
         [SerializeField] private SystemMap systemMap;
-        
+
         private OrbitalMovement _orbitalMovement;
-        
+
         private bool _isMoving = false;
         private Vector3 _targetPositionLocal;
         private Transform _targetBodyTransform;
-        
+
         private float _distanceLimit = 0.001f;
 
         private void Awake()
@@ -42,7 +50,7 @@ namespace ProceduralPlanets.Movement
         {
             systemMap.OnBodySelected += MoveToBody;
         }
-        
+
         private void OnDisable()
         {
             systemMap.OnBodySelected -= MoveToBody;
@@ -50,67 +58,122 @@ namespace ProceduralPlanets.Movement
 
         private void MoveToBody(Vector2 targetBodyIndex)
         {
-            CelestialBodyGeneratorBase targetBody = systemGenerator.GetBodyByIndex(targetBodyIndex);
-            
             if (_isMoving) return;
-            
+
+            CelestialBodyGeneratorBase targetBody = systemGenerator.GetBodyByIndex(targetBodyIndex);
+
             _orbitalMovement.enabled = false;
 
-            transform.SetParent(targetBody.transform, true);
             _targetBodyTransform = targetBody.transform;
-            _targetPositionLocal = CalculateTargetPosition(targetBody);
+            _targetPositionLocal = Vector3.zero;
 
             _isMoving = true;
-            StartCoroutine(Move(targetBody.GetBodyData().Radius * orbitToBodyRadiusRatio));
+            StartCoroutine(ExecuteFlight(targetBody.GetBodyData().Radius * orbitToBodyRadiusRatio));
         }
 
-        private Vector3 CalculateTargetPosition(CelestialBodyGeneratorBase targetBody)
+        private Vector3 CalculateTargetPosition(Transform targetBody, float orbitRadius)
         {
-            var bodyRadius = targetBody.GetBodyData().Radius;
-            
-            var direction = Vector3.ProjectOnPlane(transform.localPosition.normalized, targetBody.transform.up); 
-            
-            var targetPosition = direction * bodyRadius * orbitToBodyRadiusRatio;
+            var positionRelativeToTarget = targetBody.transform.InverseTransformPoint(transform.position);
+
+            var direction = Vector3.ProjectOnPlane(positionRelativeToTarget.normalized, targetBody.transform.up);
+
+            var targetPosition = direction * orbitRadius;
             return targetPosition;
         }
 
-        private IEnumerator Move(float radius)
+        private IEnumerator ExecuteFlight(float radius)
         {
-            var currentDistance = Vector3.Distance(transform.localPosition, _targetPositionLocal);
             var decelerationDistance = radius * decelerationDistanceToRadiusRatio;
             
-            while (currentDistance > _distanceLimit)
-            {
-                currentDistance = Vector3.Distance(transform.localPosition, _targetPositionLocal);
-                
-                var currentVelocity = velocity;
-                
-                if (currentDistance < decelerationDistance)
-                {
-                    var t = currentDistance / decelerationDistance;
-                    t = Mathf.SmoothStep(0f, 1f, t);
-                    currentVelocity = Mathf.Lerp(minVelocity, velocity, t);
-                }
-                
-                var step = currentVelocity * Time.deltaTime;
-                transform.localPosition = Vector3.MoveTowards(transform.localPosition, _targetPositionLocal, step);
-                yield return null;
-            }
+            yield return StartCoroutine(Accelerate(radius));
+
+            _targetPositionLocal = CalculateTargetPosition(_targetBodyTransform, radius);
+            transform.SetParent(_targetBodyTransform, true);
+
+            yield return StartCoroutine(Coast(decelerationDistance));
+
+            yield return StartCoroutine(Decelerate(radius));
+
             transform.localPosition = _targetPositionLocal;
             _isMoving = false;
+
+            OrbitTargetBody(radius);
+        }
+
+        private void OrbitTargetBody(float radius)
+        {
             var newOrbit = new OrbitParameters(radius, 1f, 0f, 0f, orbitalVelocity);
-            
             _orbitalMovement.SetParameters(newOrbit);
             _orbitalMovement.CalculateAngleFromPositionForPerfectOrbit();
             _orbitalMovement.enabled = true;
         }
 
+        private IEnumerator Accelerate(float decelerationDistance)
+        {
+            float currentVelocity = 0f;
+            float elapsedTime = 0f;
+
+            while (currentVelocity < velocity)
+            {
+                var currentDistance = Vector3.Distance(transform.position,
+                    _targetBodyTransform.TransformPoint(_targetPositionLocal));
+                if (currentDistance <= decelerationDistance) yield break;
+
+                elapsedTime += Time.deltaTime;
+                var t = elapsedTime / accelerationDuration;
+
+                currentVelocity = Mathf.SmoothStep(0f, velocity, t);
+
+                MoveOneStepTowardsTargetWorld(currentVelocity);
+                yield return null;
+            }
+        }
+
+        private IEnumerator Coast(float decelerationDistance)
+        {
+            while (Vector3.Distance(transform.localPosition, _targetPositionLocal) > decelerationDistance)
+            {
+                MoveOneStepTowardsTargetLocal(velocity);
+                yield return null;
+            }
+        }
+
+        private IEnumerator Decelerate(float radius)
+        {
+            var currentDistance = Vector3.Distance(transform.localPosition, _targetPositionLocal);
+            var decelerationDistance = radius * decelerationDistanceToRadiusRatio;
+
+            while (currentDistance > _distanceLimit)
+            {
+                currentDistance = Vector3.Distance(transform.localPosition, _targetPositionLocal);
+
+                var t = currentDistance / decelerationDistance;
+                t = Mathf.SmoothStep(0f, 1f, t);
+                var currentVelocity = Mathf.Lerp(minVelocity, velocity, t);
+
+                MoveOneStepTowardsTargetLocal(currentVelocity);
+                yield return null;
+            }
+        }
+
+        private void MoveOneStepTowardsTargetLocal(float currentVelocity)
+        {
+            var step = currentVelocity * Time.deltaTime;
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _targetPositionLocal, step);
+        }
+
+        private void MoveOneStepTowardsTargetWorld(float currentVelocity)
+        {
+            var step = currentVelocity * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, _targetBodyTransform.position, step);
+        }
+
         private void OnDrawGizmos()
         {
             if (!_isMoving) return;
-            
+
             Vector3 targetPositionWorld = _targetBodyTransform.TransformPoint(_targetPositionLocal);
-            
+
             Gizmos.color = trajectoryColor;
             Gizmos.DrawLine(transform.position, targetPositionWorld);
             Gizmos.color = destinationColor;

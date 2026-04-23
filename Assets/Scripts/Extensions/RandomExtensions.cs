db4cb7a5403bf27f1f8c77409952b3b8489ebcc3
Yakov Balashov <yakov.balashov@gmail.com>
2026-04-23 12:33:30 +0200

    feat: add overrides for random range

--- Assets/Scripts/Extensions/RandomExtensions.cs
+++ Assets/Scripts/Extensions/RandomExtensions.cs
@@ -8,5 +8,13 @@ namespace ProceduralPlanets.Extensions
         {
             return (float)(range.x + random.NextDouble() * (range.y - range.x));
         }
+        public static float Range(this System.Random random, float minValue, float maxValue)
+        {
+            return (float)(minValue + random.NextDouble() * (maxValue - minValue));
+        }
+        public static int Range(this System.Random random, int minValue, int maxValue)
+        {
+            return random.Next(minValue, maxValue);
+        }
     }
 }

e3ad7202f347bc9f1f3e6c2ab79e004cffbdf7b1
Yakov Balashov <yakov.balashov@gmail.com>
2026-04-25 15:14:26 +0200

    feat: scale player to simulate zoom

--- Assets/Scripts/Camera/OrbitCamera.cs
+++ Assets/Scripts/Camera/OrbitCamera.cs
@@ -5,11 +5,20 @@ using UnityEngine.InputSystem;
 
 namespace ProceduralPlanets.Camera
 {
+    [RequireComponent(typeof(CinemachineCamera))]
     public class OrbitalCameraActionManager : MonoBehaviour
     {
+        [SerializeField] private Transform playerTransform;
+
         [SerializeField] private InputActionReference toggleOrbitAction;
         [SerializeField] private InputActionReference lookAction;
         [SerializeField] private InputActionReference zoomAction;
+
+        [SerializeField] private float minScale = 0.1f;
+        [SerializeField] private float maxScale = 5.0f;
+        [SerializeField] private float zoomSensitivity = 0.01f;
+
+        private const float MinScroll = 0.001f;
 
         private void OnEnable()
         {
@@ -17,6 +26,7 @@ namespace ProceduralPlanets.Camera
             zoomAction.action.Enable();
             toggleOrbitAction.action.performed += EnableOrbit;
             toggleOrbitAction.action.canceled += DisableOrbit;
+            zoomAction.action.performed += ScalePlayer;
         }
 
         private void OnDisable()
@@ -26,6 +36,18 @@ namespace ProceduralPlanets.Camera
             lookAction.action.Disable();
             toggleOrbitAction.action.performed -= EnableOrbit;
             toggleOrbitAction.action.canceled -= DisableOrbit;
+            zoomAction.action.performed -= ScalePlayer;
+        }
+
+        private void ScalePlayer(InputAction.CallbackContext context)
+        {
+            float scrollValue = context.ReadValue<Vector2>().y;
+
+            if (Math.Abs(scrollValue) < MinScroll) return;
+
+            float currentScale = playerTransform.localScale.x;
+            float newScale = Mathf.Clamp(currentScale + scrollValue * zoomSensitivity, minScale, maxScale);
+            playerTransform.localScale = new Vector3(newScale, newScale, newScale);
         }
 
         private void EnableOrbit(InputAction.CallbackContext context)

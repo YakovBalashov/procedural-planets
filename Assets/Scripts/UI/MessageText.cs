a7fbbd095f1f91f63b7c2958f0e28d7e3d2ba3c0
Yakov Balashov <yakov.balashov@gmail.com>
2026-05-06 19:23:23 +0200

    feat: implement message box

---
+++ Assets/Scripts/UI/MessageText.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using ProceduralPlanets.Generation;
+using TMPro;
+using UnityEngine;
+
+namespace ProceduralPlanets.UI
+{
+    public class MessageText : MonoBehaviour
+    {
+        public static Color ErrorColor => Color.lightCoral;
+        public static Color SuccessColor => Color.lightGreen;
+        public static Color InfoColor => Color.lightBlue;
+        
+        private TextMeshProUGUI _text;
+        private bool _isMessageDisplaying;
+        
+        private void Awake()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            SystemGenerator.MessageText = this;
+        }
+        
+        public void SetMessage(string message, float durationInSeconds, Color color)
+        {
+            if (_isMessageDisplaying) StopAllCoroutines();
+            _text.text = message;
+            _text.color = color;
+            StartCoroutine(EraseMessageAfterDelay(durationInSeconds));
+        }
+        
+        private IEnumerator EraseMessageAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            _text.text = string.Empty;
+        }
+    }
+}
